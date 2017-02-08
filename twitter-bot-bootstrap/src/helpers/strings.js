5e5655b11f56604a7e6804eea40fa4eac96a568d
Kristin Anthony <anthkris09@gmail.com>
2017-02-08 06:39:03 +0000

    Unsubmodule twitter bot

---
+++ twitter-bot-bootstrap/src/helpers/strings.js
@@ -0,0 +1,61 @@
+module.exports = {
+  queryString: [
+    'seriousgame'
+  ],
+  queryStringSubQuery: [
+    ''
+  ],
+  blockedStrings: [
+    'porn',
+    'sex',
+    'fuck',
+    'shit',
+    'pussy',
+    'dick',
+    'cock',
+    'nigger',
+    'nigga',
+    'bitch',
+    'hoe',
+    'cuck'
+  ],
+  resultType: [
+    'mixed',
+    'recent',
+    'popular'
+  ],
+  responseString: [
+    'Hi @screenName thanks for the follow! If you need a project idea, tweet at me! !CR',
+    '@screenName thanks for following! Tweet at me for a #instructionaldesign project idea. !CR',
+    'Awesome @screenName, thanks for following! Tweet at me for a #instructionaldesign project idea. !CR'
+  ],
+  ideaString: [
+    ' Here\'s an idea:\nTry making a game that teaches players how to insult a person in Shakesperian English.',
+    ' Try this:\nDevelop a mini game about avoiding phishing emails.',
+    ' Here ya go:\nDevelop a list of personas for a course or game you\'re going to make.',
+    ' Try this:\nDevelop a checklist/job aid for first time apartment renters.',
+    ' Here\'s an idea:\nDesign an interactive map of landmark eateries in Los Angeles, CA to train new tour guides.',
+    ' Here\'s an idea:\nDevelop an interactive quiz to show an instructional designer what salary range they can expect.',
+    ' Try this:\nCreate a mockup of a website that will accept and arrange user-created content around an instructional topic.',
+    ' How\'s this:\nCreate a mobile interactive job aid for new drivers on how to check their oil.',
+    ' How\'s this:\nDevelop an email onboarding series for new instructional design hires at your company.',
+    ' Be civic-minded!\nTry your hand at redesigning a water quality report to be meaningful to lay people.',
+    ' Here ya go:\nCreate a simulation to help newly hired cashiers check for counterfeit currency.',
+    ' Here ya go:\nDesign a face-to-face training activity for new dog walking volunteers on approaching the kennel for a walk.',
+    ' How\'s this:\nDevelop a video or interactive on how to read your utility bill.',
+    ' Try this one on for size:\nCreate an interactive about the parts of an DSLR camera.',
+    ' Try this:\nCurate or design a subscription learning course on finding your first apartment.',
+    ' Try this one:\nCreate and document an interactive template for faculty using a rapid elearning tool.',
+    ' How\'s this:\nDevelop a badge system for a course you\'ve designed or taken.',
+    ' Here ya go:\nCreate a visual language for courses/materials on an instructional topic.',
+    ' Here ya go:\nDevelop a style guide or mood board for a course you\'re planning to make.',
+    ' Try this:\nDevelop a storyboard for an interactive conversation on asking for a raise.',
+    ' Try this:\nDevelop an interactive conversation that trains users on how to ask for a raise.',
+    ' Try this:\nCreate a checklist or other job aid on making accessible interactions in your rapid elearning tool.',
+    ' How\'s this:\nDevelop a subscription learning course for SMEs on applying what we know about how people learn.',
+    ' How\'s this:\nCreate a course on using Trello (or another kanban board tool) for personal learning.',
+    ' How\'s this:\nCreate a video on connecting services using Zapier.',
+    ' Try this one on for size:\nCreate a course on digital minimalism for incoming freshman college students.',
+    ' Here ya go:\nDevelop an interactive map of a fictional journey. Think LOTR or Narnia.',
+  ],
+}
